fa7b3b31a83f5e914ddba7a9887bba9fc03302b1
Alberto <mail4alberto@gmail.com>
2015-11-22 04:52:26 -0500

    clean up

--- src/main.rs
+++ src/main.rs
@@ -147,42 +147,4 @@ fn main() {
         Ok(_) => println!("Listening on port 3030"),
     }
 
-
-    // TODO: should be from command line
-    // let path_str = "test.db";
-
-    // let utc: DateTime<UTC> = UTC::now();
-
-    // println!("{}", utc.timestamp().to_string());
-
-
-    // conn.execute("CREATE TABLE person (
-    //               id              INTEGER PRIMARY KEY,
-    //               name            TEXT NOT NULL,
-    //               time_created    TEXT NOT NULL,
-    //               data            BLOB
-    //               )", &[]).unwrap();
-    // let me = Person {
-    //     id: 0,
-    //     name: "Steven".to_string(),
-    //     time_created: time::get_time(),
-    //     data: None
-    // };
-    // conn.execute("INSERT INTO person (name, time_created, data)
-    //               VALUES ($1, $2, $3)",
-    //              &[&me.name, &me.time_created, &me.data]).unwrap();
-
-    // let mut stmt = conn.prepare("SELECT id, name, time_created, data FROM person").unwrap();
-    // let mut person_iter = stmt.query_map(&[], |row| {
-    //     Person {
-    //         id: row.get(0),
-    //         name: row.get(1),
-    //         time_created: row.get(2),
-    //         data: row.get(3)
-    //     }
-    // }).unwrap();
-
-    // for person in person_iter {
-    //     println!("Found person {:?}", person.unwrap());
-    // }
 }
