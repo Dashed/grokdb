25b9ebe78eb2d122efd8afba2dd2289dcb2e6409
Alberto <mail4alberto@gmail.com>
2015-11-22 23:50:49 -0500

    custom sqlite scalar function: rank_score

--- src/database.rs
+++ src/database.rs
@@ -1,12 +1,16 @@
+extern crate libc;
 extern crate rusqlite;
+extern crate libsqlite3_sys as ffi;
 
 use std::error;
 use std::fmt;
 use std::ops::Deref;
 use std::sync::{Arc, Mutex, LockResult, MutexGuard};
+use libc::{c_int, c_double};
 
 use rusqlite::SqliteConnection;
 use rusqlite::SqliteError;
+use rusqlite::functions::{sqlite3_context, sqlite3_value, FromValue,ToResult};
 
 use queries::tables;
 
@@ -117,6 +121,15 @@ pub fn bootstrap(database_name: String) -> Result<DB, BootstrapError> {
             return Err(BootstrapError::Sqlite(why));
         },
         Ok(db_conn) => {
+
+            // TODO: move this somewhere
+            match db_conn.create_scalar_function("rank_score", 4, true, Some(rank_score)) {
+                Err(why) => {
+                    return Err(BootstrapError::Sqlite(why));
+                },
+                _ => {}
+            }
+
             let lock = Mutex::new(db_conn);
             let arc = Arc::new(lock).clone();
 
@@ -166,4 +179,66 @@ fn create_tables(db: &DB) -> Result<(), QueryError> {
     }
 
     return Ok(());
+}
+
+// TODO: move this somewhere
+extern "C" fn rank_score(ctx: *mut sqlite3_context, _: c_int, argv: *mut *mut sqlite3_value) {
+
+    // rank_score(success: int, fail: int, age: int, times_reviewed: int) -> f64
+
+    unsafe {
+
+        let success = {
+            let _success = *argv.offset(0);
+
+            if !c_int::parameter_has_valid_sqlite_type(_success) {
+                ffi::sqlite3_result_error_code(ctx, ffi::SQLITE_MISMATCH);
+                return;
+            }
+
+            c_int::parameter_value(_success).unwrap() as c_double
+        };
+
+        let fail = {
+            let _fail = *argv.offset(1);
+
+            if !c_int::parameter_has_valid_sqlite_type(_fail) {
+                ffi::sqlite3_result_error_code(ctx, ffi::SQLITE_MISMATCH);
+                return;
+            }
+
+            c_int::parameter_value(_fail).unwrap() as c_double
+        };
+
+        let age = {
+            let _age = *argv.offset(2);
+
+            if !c_int::parameter_has_valid_sqlite_type(_age) {
+                ffi::sqlite3_result_error_code(ctx, ffi::SQLITE_MISMATCH);
+                return;
+            }
+
+            c_int::parameter_value(_age).unwrap() as c_double
+        };
+
+        let times_reviewed = {
+            let _times_reviewed = *argv.offset(3);
+
+            if !c_int::parameter_has_valid_sqlite_type(_times_reviewed) {
+                ffi::sqlite3_result_error_code(ctx, ffi::SQLITE_MISMATCH);
+                return;
+            }
+
+            c_int::parameter_value(_times_reviewed).unwrap() as c_double
+        };
+
+        let total: c_double = success + fail;
+
+        let lidstone: c_double = (fail + 0.5f64) / (total + 1.0f64);
+        let bias_factor: c_double = (1.0f64 + fail) / ((total + 1.0f64) + success + times_reviewed / 3.0f64);
+        let base: c_double = lidstone + 1.0f64;
+
+        let rank_score: c_double = lidstone * (age * bias_factor + base).ln() / base.ln();
+        rank_score.set_result(ctx);
+    }
 }
