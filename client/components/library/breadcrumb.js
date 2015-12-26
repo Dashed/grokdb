3a2fe63082bfc694e499a36d1765ff19cc6567c3
Alberto <mail4alberto@gmail.com>
2015-12-26 02:10:34 -0500

    breadcrumb polish

--- client/components/library/breadcrumb.js
+++ client/components/library/breadcrumb.js
@@ -65,12 +65,14 @@ const Breadcrumb = React.createClass({
     }
 });
 
+// this is a placeholder component on initial load/mount to occupy the space
+// that the component will cover in order to prevent any inducement of jank.
 const BreadcrumbWaiting = React.createClass({
     render() {
         return (
             <ol className="breadcrumb m-y-0">
                 <li className="active" style={{color: '#eceeef'}}>
-                    {'loading'}
+                    {'.'}
                 </li>
             </ol>
         );
