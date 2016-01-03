420a18c37a1434471e3207b1fa5e771ab02612f6
Alberto <mail4alberto@gmail.com>
2016-01-02 22:34:45 -0500

    back to stash list from add stash

--- client/components/stashes/create.js
+++ client/components/stashes/create.js
@@ -4,10 +4,29 @@ const courier = require('courier');
 
 const CreateStash = React.createClass({
 
+    contextTypes: {
+        store: React.PropTypes.object.isRequired
+    },
+
+    backToStashesList(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        this.context.store.routes.toStashes();
+    },
+
     render() {
         return (
             <div>
-                {'CreateStash'}
+                <div className="row">
+                    <div className="col-sm-12 m-b">
+                        <button
+                            type="button"
+                            className="btn btn-sm btn-danger"
+                            onClick={this.backToStashesList}
+                        >{'Back to decks list'}</button>
+                    </div>
+                </div>
             </div>
         );
     }
