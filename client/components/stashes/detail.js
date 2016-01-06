5758763c74101cdf167ac09d0069266d1809bd17
Alberto <mail4alberto@gmail.com>
2016-01-06 10:21:36 -0500

    back to stash list from stash detail

--- client/components/stashes/detail.js
+++ client/components/stashes/detail.js
@@ -2,10 +2,29 @@ const React = require('react');
 
 const StashDetail = React.createClass({
 
+    contextTypes: {
+        store: React.PropTypes.object.isRequired
+    },
+
+    backToStashesList(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        this.context.store.routes.toStashes();
+    },
+
     render() {
         return (
             <div>
-                {'stash detail'}
+                <div className="row">
+                    <div className="col-sm-12 m-b">
+                        <button
+                            type="button"
+                            className="btn btn-sm btn-danger"
+                            onClick={this.backToStashesList}
+                        >{'Back to list of stashes'}</button>
+                    </div>
+                </div>
             </div>
         );
     }
