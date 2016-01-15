7bee2df133040031324335bbdd58b9ed4e6e68c3
Alberto <mail4alberto@gmail.com>
2016-01-14 19:27:06 -0500

    reduce co usage

--- client/store/bootstrap.js
+++ client/store/bootstrap.js
@@ -1,20 +1,16 @@
-const co = require('co');
-
 const {bootstrap: bootstrapRoutes} = require('./routes');
 
 
 module.exports = function(store) {
 
-    return co(function* () {
-
-        yield bootstrapRoutes(store);
-
-        return store;
-
-    }).catch(function(err) {
-        // TODO: proper error logging
-        console.error(err);
-        console.error(err.stack);
-    });
+    return bootstrapRoutes(store)
+        .then(function() {
+            return store;
+        })
+        .catch(function(err) {
+            // TODO: proper error logging
+            console.error(err);
+            console.error(err.stack);
+        });
 
 };
