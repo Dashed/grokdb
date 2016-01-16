aa4518fa47bfd32f9c64ad77a04f7dd8ea917bf6
Alberto <mail4alberto@gmail.com>
2016-01-16 05:36:13 -0500

    polish

--- buildrun.sh
+++ buildrun.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 PKG_CONFIG_PATH=$(echo /usr/local/Cellar/sqlite/3.9.1/lib/pkgconfig/) cargo build
-target/debug/grokdb --app=./assets utsc -p 3030 --dir=./images
+target/debug/grokdb --app=./assets -p 3030 --dir=./images utsc
