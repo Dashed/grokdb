db8fd93ce2b5977f0006f17954c70bf64389b444
Alberto <mail4alberto@gmail.com>
2016-01-16 23:06:12 -0500

    as of rusqlite v0.5.0 setting `PKG_CONFIG_PATH` is no longer necessary

--- buildrun.sh
+++ buildrun.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-PKG_CONFIG_PATH=$(echo /usr/local/Cellar/sqlite/3.9.1/lib/pkgconfig/) cargo build
+# see: https://github.com/jgallagher/rusqlite/blob/master/Changelog.md#version-050-2015-12-08
+# PKG_CONFIG_PATH=$(echo /usr/local/Cellar/sqlite/3.9.1/lib/pkgconfig/) cargo build
+
+cargo build
 target/debug/grokdb --app=./assets -p 3030 --dir=./images utsc
